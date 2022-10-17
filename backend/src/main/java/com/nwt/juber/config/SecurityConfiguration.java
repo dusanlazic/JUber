8cdec7e0ab951849de84b6ddeda2184fc53d3974
Dušan Lazić <lazicdusan1104@gmail.com>
2022-10-17 19:53:01 +0200

    Configure h2 database

---
+++ backend/src/main/java/com/nwt/juber/config/SecurityConfiguration.java
@@ -0,0 +1,15 @@
+package com.nwt.juber.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/h2-console/**", "/swagger-ui.html");
+    }
+
+}

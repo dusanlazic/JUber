6d72e785ea71e5e584391ffd282a5ec972c38ebf
Dušan Lazić <lazicdusan1104@gmail.com>
2022-10-17 19:37:00 +0200

    Initialize Spring Boot project

---
+++ backend/src/main/java/com/nwt/juber/JuberApplication.java
@@ -0,0 +1,13 @@
+package com.nwt.juber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuberApplication.class, args);
+	}
+
+}

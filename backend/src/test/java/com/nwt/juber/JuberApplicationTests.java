feccf6a17ed48ee9a9a006e6148c12e0c5ff9719
Dušan Lazić <lazicdusan1104@gmail.com>
2022-10-17 19:37:00 +0200

    Initialize Spring Boot project

---
+++ backend/src/test/java/com/nwt/juber/JuberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwt.juber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

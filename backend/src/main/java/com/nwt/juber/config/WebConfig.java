6ec5465dcc4a953e6de7784ef5c884d4cdd2f87d
Dušan Lazić <lazicdusan1104@gmail.com>
2022-10-17 20:25:50 +0200

    Configure CORS

---
+++ backend/src/main/java/com/nwt/juber/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.nwt.juber.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://127.0.0.1:3000",
+                        "http://localhost:3000")
+                .allowedMethods(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "PATCH",
+                        "DELETE",
+                        "OPTIONS");
+    }
+}

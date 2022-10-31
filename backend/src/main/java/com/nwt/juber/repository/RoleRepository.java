2818e8cf8738cc22fc6bbb8ed154cf792ef1f36a
Dušan Lazić <lazicdusan1104@gmail.com>
2022-10-31 20:49:49 +0100

    Add User and Role repositories

---
+++ backend/src/main/java/com/nwt/juber/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.nwt.juber.repository;
+
+import com.nwt.juber.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByTitle(String title);
+
+}

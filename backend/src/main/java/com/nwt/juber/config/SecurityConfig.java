2354e76e992957f9cf584e11ddf6f465f66474b5
Dušan Lazić <lazicdusan1104@gmail.com>
2022-10-31 20:52:31 +0100

    Rename config class to SecurityConfig

--- backend/src/main/java/com/nwt/juber/config/SecurityConfig.java
+++ backend/src/main/java/com/nwt/juber/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 @Configuration
-public class SecurityConfiguration {
+public class SecurityConfig {
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {

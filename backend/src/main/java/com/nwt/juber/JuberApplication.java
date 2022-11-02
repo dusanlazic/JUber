c54187d8c79aaa5ae7352357264694c443071161
Dušan Lazić <lazicdusan1104@gmail.com>
2022-11-03 00:31:45 +0100

    Add configuration for jwt, cors and oauth redirects

--- backend/src/main/java/com/nwt/juber/JuberApplication.java
+++ backend/src/main/java/com/nwt/juber/JuberApplication.java
@@ -1,9 +1,12 @@
 package com.nwt.juber;
 
+import com.nwt.juber.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class JuberApplication {
 
 	public static void main(String[] args) {

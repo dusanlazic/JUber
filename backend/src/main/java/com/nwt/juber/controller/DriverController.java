a98862776d1464582e3a012e1440399d7f354180
Pavle <pavleg.19@gmail.com>
2022-11-03 16:05:19 +0100

    Create FastAPI application

--- backend/src/main/java/com/nwt/juber/controller/DriverController.java
+++ backend/src/main/java/com/nwt/juber/controller/DriverController.java
@@ -5,7 +5,6 @@ import com.nwt.juber.dto.LocationDTO;
 import com.nwt.juber.dto.SimulationInfo;
 import com.nwt.juber.service.DriverService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;

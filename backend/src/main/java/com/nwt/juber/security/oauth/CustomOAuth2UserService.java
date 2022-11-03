2a49210db46688b4c6262435098b43e3d00572d0
Dušan Lazić <lazicdusan1104@gmail.com>
2022-11-03 01:40:10 +0100

    Add missing Service annotation

--- backend/src/main/java/com/nwt/juber/security/oauth/CustomOAuth2UserService.java
+++ backend/src/main/java/com/nwt/juber/security/oauth/CustomOAuth2UserService.java
@@ -13,10 +13,12 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.util.Optional;
 
+@Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     @Autowired

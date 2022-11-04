c3fd9faa9eeaa294cb933663433fc3d29d32356a
Dušan Lazić <lazicdusan1104@gmail.com>
2022-11-04 23:07:36 +0100

    Fix enumeration naming

--- backend/src/main/java/com/nwt/juber/security/oauth/user/OAuth2UserInfoFactory.java
+++ backend/src/main/java/com/nwt/juber/security/oauth/user/OAuth2UserInfoFactory.java
@@ -2,7 +2,6 @@ package com.nwt.juber.security.oauth.user;
 
 import com.nwt.juber.exception.NotImplementedException;
 import com.nwt.juber.model.AuthProvider;
-import com.nwt.juber.model.AuthProvider.*;
 
 import java.util.Map;
 
@@ -10,8 +9,8 @@ public class OAuth2UserInfoFactory {
 
     public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
         return switch (AuthProvider.valueOf(registrationId)) {
-            case GOOGLE -> new GoogleOAuth2UserInfo(attributes);
-            case FACEBOOK -> throw new NotImplementedException("Facebook login is not implemented yet.");
+            case google -> new GoogleOAuth2UserInfo(attributes);
+            case facebook -> throw new NotImplementedException("Facebook login is not implemented yet.");
             default -> throw new IllegalArgumentException("Unknown provider type.");
         };
     }

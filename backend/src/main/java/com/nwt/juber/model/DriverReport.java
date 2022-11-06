340e0ea2fcbb907ae118e6039289f4ddc97bc42e
Dušan Lazić <lazicdusan1104@gmail.com>
2022-11-06 04:15:00 +0100

    Fix H2 and UUID issues

--- backend/src/main/java/com/nwt/juber/model/DriverReport.java
+++ backend/src/main/java/com/nwt/juber/model/DriverReport.java
@@ -3,6 +3,7 @@ package com.nwt.juber.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.UUID;
@@ -11,6 +12,8 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 public class DriverReport {
+
     @Id
+    @Column(columnDefinition = "uuid")
     private UUID id;
 }

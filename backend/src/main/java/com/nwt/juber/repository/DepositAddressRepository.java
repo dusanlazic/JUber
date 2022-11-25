ba8c921a312fcdf2719219667ee4c0d5a4db527b
Dušan Lazić <lazicdusan1104@gmail.com>
2022-11-25 03:10:08 +0100

    Add DepositAddressStatus

--- backend/src/main/java/com/nwt/juber/repository/DepositAddressRepository.java
+++ backend/src/main/java/com/nwt/juber/repository/DepositAddressRepository.java
@@ -1,6 +1,7 @@
 package com.nwt.juber.repository;
 
 import com.nwt.juber.model.DepositAddress;
+import com.nwt.juber.model.DepositAddressStatus;
 import com.nwt.juber.model.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,8 +12,16 @@ import java.util.UUID;
 @Repository
 public interface DepositAddressRepository extends JpaRepository<DepositAddress, UUID> {
 
-    Optional<DepositAddress> findFirstByPassengerAndPaidWeiIsNull(Passenger passenger);
+    Optional<DepositAddress> findFirstByPassengerAndStatus(Passenger passenger, DepositAddressStatus status);
 
-    Optional<DepositAddress> findFirstByPassengerIsNull();
+    default Optional<DepositAddress> findFirstByPassengerAndIsPending(Passenger passenger) {
+        return findFirstByPassengerAndStatus(passenger, DepositAddressStatus.PENDING);
+    }
+
+    Optional<DepositAddress> findFirstByStatus(DepositAddressStatus status);
+
+    default Optional<DepositAddress> findFirstUnassigned() {
+        return findFirstByStatus(DepositAddressStatus.UNASSIGNED);
+    }
 
 }

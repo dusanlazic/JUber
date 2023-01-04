97a4fd496a9171ac00d71008751fb3bb8c3ca815
Dušan Lazić <lazicdusan1104@gmail.com>
2023-01-04 19:58:28 +0100

    Model message classes

---
+++ backend/src/main/java/com/nwt/juber/model/ChatConversation.java
@@ -0,0 +1,30 @@
+package com.nwt.juber.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class ChatConversation {
+
+    @Id
+    @Column(columnDefinition = "uuid")
+    private UUID id = UUID.randomUUID();
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Admin support;
+
+    @OneToMany(cascade = CascadeType.PERSIST)
+    private List<PersistedChatMessage> messages;
+
+    private Boolean isArchived;
+
+}
---
+++ backend/src/main/java/com/nwt/juber/model/PersistedChatMessage.java
@@ -0,0 +1,29 @@
+package com.nwt.juber.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class PersistedChatMessage {
+
+    @Id
+    @Column(columnDefinition = "uuid")
+    private UUID id = UUID.randomUUID();
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private ChatConversation conversation;
+
+    private String content;
+
+    private Boolean isFromSupport;
+
+    @CreationTimestamp
+    private Date sentAt;
+}

d915df1ff9e890ad543e5a668eeca41770df4a7f
KatarinaKomad <komad.katarina@gmail.com>
2023-01-21 15:22:46 +0100

    Improved guard redirections

--- frontend/src/services/auth/role-guard.service.ts
+++ frontend/src/services/auth/role-guard.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot } from '@angular/router';
 import { AuthService } from './auth.service';
-import decode from 'jwt-decode';
 import { Toastr } from '../util/toastr.service';
 import { LocalStorageService } from '../util/local-storage.service';
+import { Location } from '@angular/common';
 
 @Injectable({
     providedIn: 'root'
@@ -14,14 +14,13 @@ export class RoleGuardService implements CanActivate {
     constructor(
         public authService: AuthService, 
         public router: Router, 
-        private toastr: Toastr,
+        public location: Location,
         private localStorage: LocalStorageService
     ) { }
 
     canActivate(route: ActivatedRouteSnapshot): boolean {
         if(!this.isAuthenticated(route)){
-            this.toastr.error("You do not have valid permisions")
-            this.router.navigate(['']);
+            this.location.back();
             return false;
         }
         return true;

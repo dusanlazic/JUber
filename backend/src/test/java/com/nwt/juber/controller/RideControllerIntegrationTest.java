a7f9b56cbd4300a3f84d79ad964d96eb9048487a
Pavle <pavleg.19@gmail.com>
2023-01-27 16:24:39 +0100

    Add another test

--- backend/src/test/java/com/nwt/juber/controller/RideControllerIntegrationTest.java
+++ backend/src/test/java/com/nwt/juber/controller/RideControllerIntegrationTest.java
@@ -40,6 +40,16 @@ public class RideControllerIntegrationTest {
 
 	HttpHeaders passengerHeaders = new HttpHeaders();
 
+	public void login() {
+		LoginRequest request = new LoginRequest();
+		request.setEmail(passengerUsername);
+		request.setPassword(passengerPassword);
+		ResponseEntity<TokenResponse> token = restTemplate
+				.exchange("/auth/login", HttpMethod.POST, new HttpEntity<>(request), TokenResponse.class);
+		passengerToken = token.getBody().getAccessToken();
+		passengerHeaders.setBearerAuth(passengerToken);
+	}
+
 	@Test
 	public void health() {
 		login();
@@ -50,7 +60,7 @@ public class RideControllerIntegrationTest {
 	}
 
 	@ParameterizedTest
-	@ValueSource(strings = {"8107614c-04d9-480d-8a59-e1999d9e7bfc"})
+	@ValueSource(strings = {"8107614c-04d9-480d-8a59-e1999d9e7bfc", "46ab4aff-d171-4447-a05c-204a29d0fde1"})
 	public void Ride_with_id(String id) {
 		login();
 		Ride ride = rideRepository.findById(UUID.fromString(id)).get();
@@ -63,13 +73,16 @@ public class RideControllerIntegrationTest {
 
 	}
 
-	public void login() {
-		LoginRequest request = new LoginRequest();
-		request.setEmail(passengerUsername);
-		request.setPassword(passengerPassword);
-		ResponseEntity<TokenResponse> token = restTemplate
-				.exchange("/auth/login", HttpMethod.POST, new HttpEntity<>(request), TokenResponse.class);
-		passengerToken = token.getBody().getAccessToken();
-		passengerHeaders.setBearerAuth(passengerToken);
+
+	@Test
+	public void Active_ride() {
+		login();
+		HttpEntity<String> entity = new HttpEntity<>(passengerHeaders);
+		ResponseEntity<RideDTO> responseEntity = restTemplate.exchange("/ride/active", HttpMethod.GET,  entity,RideDTO.class);
+		assert responseEntity.getBody() != null;
+		assert responseEntity.getStatusCode() == HttpStatus.OK;
+		assert responseEntity.getBody().getId().toString().equals("2ac4bc01-6326-418f-a3f9-4244e3922439");
 	}
+
+
 }

ada1c9e8c0eb48303d450aabb8fc67cd3ed0103c
Dušan Lazić <lazicdusan1104@gmail.com>
2023-02-01 21:23:15 +0100

    Add validations

--- backend/src/main/java/com/nwt/juber/dto/request/ride/RouteDTO.java
+++ backend/src/main/java/com/nwt/juber/dto/request/ride/RouteDTO.java
@@ -3,12 +3,27 @@ package com.nwt.juber.dto.request.ride;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Data
 @AllArgsConstructor
 public class RouteDTO {
+    @NotBlank
     private String name;
+
+    @NotNull
+    @Positive
     private Double distance;
+
+    @NotNull
+    @Positive
     private Double duration;
+
+    @NotBlank
     private String coordinatesEncoded;
+
+    @NotNull
     private Boolean selected;
 }
